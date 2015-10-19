39bff69e90998fbcbbdc6de10c4ab7836d74419f
bgaynor78 <Rose@Roses-MacBook-Pro-2.local>
2015-10-18 23:05:19 -0500

    Lots of addtions
    Contact form (that works!), portfolio additions, fading transitions and
    more I’ve forgotten.

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,3 +1,49 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'Website Contact Form'; 
+        $to = 'brett.gaynor@gmail.com'; 
+        $subject = 'Message from Contact Form ';
+        $errName;
+        $errEmail;
+        $errMessage;
+        $errHuman;
+        
+        $body = "<strong>From:</strong> $name\n <strong>E-Mail:</strong> $email\n <strong>Message:</strong>\n $message";
+ 
+        // Check if name has been entered
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 5) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+    }
+}
+    }
+?>
+
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
@@ -14,21 +60,22 @@
         <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/animsition.min.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body id="contact">
-      <div class="bg-site-wrapper">
+      <div class="bg-site-wrapper animsition">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
           <nav>
               <ul class="list-unstyled main-menu">
                 <li class="text-right"></li>
-                <li><a href="index.html">HOME <i class="icon fa fa-home"></i></a></li>
-                <li><a href="about.html">ABOUT <i class="icon fa fa-bullhorn"></i></a></li>
-                <li><a href="work.html">WORK <i class="icon fa fa-cogs"></i></a></li>
-                <li><a href="contact.html">CONTACT <i class="icon fa fa-paper-plane"></i></a></li>
+                <li><a href="index.html" class="animsition-link">HOME <i class="icon fa fa-home"></i></a></li>
+                <li><a href="about.html" class="animsition-link">ABOUT <i class="icon fa fa-bullhorn"></i></a></li>
+                <li><a href="work.html" class="animsition-link">WORK <i class="icon fa fa-cogs"></i></a></li>
+                <li><a href="contact.html" class="animsition-link">CONTACT <i class="icon fa fa-paper-plane"></i></a></li>
               </ul>
             </nav>
 
@@ -52,20 +99,35 @@
             <p id="returnMessage"></p>
             <div class="row">
               <div class="col-sm-8 col-xs-12">
-                <form class="contact-form" id="mainContactForm">
+                <form class="contact-form" id="mainContactForm" role="form" method="post" action="contact.php">
                   <div class="form-group">
-                    <label for="nameInput" class="sr-only">Name *</label>
-                    <input type="text" class="form-control" id="nameInput" placeholder="Name *" required>
+                    <label for="name" class="sr-only">Name *</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Name *" value="<?php echo htmlspecialchars($_POST['name']); ?>" required>
+                    <?php echo "<p class='text-danger'>$errName</p>";?>
                   </div>
                   <div class="form-group">
-                    <label for="emailInput" class="sr-only">Email *</label>
-                    <input type="email" class="form-control" id="emailInput" placeholder="Email *" required>
+                    <label for="email" class="sr-only">Email *</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email *" value="<?php echo htmlspecialchars($_POST['email']); ?>" required>
+                    <?php echo "<p class='text-danger'>$errEmail</p>";?>
                   </div>
                   <div class="form-group">
-                    <label for="messageInput" class="sr-only">Message</label>
-                    <textarea class="form-control" id="messageInput" placeholder="Your Message" rows="5"></textarea>
+                    <label for="Message" class="sr-only">Message</label>
+                    <textarea class="form-control" name="message" id="message" placeholder="Your Message" rows="5"><?php echo htmlspecialchars($_POST['message']); ?></textarea>
+                    <?php echo "<p class='text-danger'>$errMessage</p>";?>
                   </div>
-                  <button type="submit" id="submit" class="btn btn-primary btn-lg">Fire Away</button>
+                  <div class="form-group clearfix">
+                    <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="human" id="human" name="human" placeholder="Your Answer">
+                        <?php echo "<p class='text-danger'>$errHuman</p>";?>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <input id="submit" name="submit" type="submit" value="Fire Away" class="btn btn-primary btn-lg">
+                  </div>
+                  <div class="form-group">
+                    <?php echo $result; ?> 
+                  </div>
                 </form>
               </div>
               <div class="col-sm-4 col-xs-12">
@@ -86,6 +148,7 @@
       </footer>       
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
+    <script src="js/vendor/animsition.min.js"></script>
     <script src="js/vendor/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
@@ -96,32 +159,6 @@
         e.src='//www.google-analytics.com/analytics.js';
         r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
         ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-    </script>
-    <script>
-      $(document).ready(function() {
-        $("#submit").click(function() {
-          var name = $("#nameInput").val();
-          var email = $("#emailInput").val();
-          var message = $("#messageInput").val();
-          $("#returnMessage").empty(); // To empty previous error/success message.
-          // Checking for blank fields.
-          if (name == '' || email == '') {
-            alert("Please Fill Required Fields");
-          } else {
-            // Returns successful data submission message when the entered information is stored in database.
-            $.post("contact_form.php", {
-              name1: name,
-              email1: email,
-              message1: message,
-            }, function(data) {
-              $("#returnMessage").append(data); // Append returned message to message paragraph.
-              if (data == "Your Query has been received, We will contact you soon.") {
-                $("#mainContactForm")[0].reset(); // To reset form fields on success.
-              }
-            });
-          }
-        });
-      });
     </script>
     </body>
 </html>
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,7 +5,29 @@
    All Rights Reserved
    ========================================================================== */
 
-$(document).ready(function(){												
+$(document).ready(function(){
+  //Fade Page Transitions
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    loading: true,
+    loadingParentElement: 'body',
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+  
+  
   //Navigation Menu Slider
   $('#nav-expander').on('click',function(e){
     e.preventDefault();
